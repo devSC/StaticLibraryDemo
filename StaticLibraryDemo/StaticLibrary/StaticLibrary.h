4ece52637eff30749044b3c8adedd65d5a25eb1b
Wilson Yuan <yuansc@nihao.com>
2017-03-08 17:34:27 +0800

    initial project

---
+++ StaticLibraryDemo/StaticLibrary/StaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibrary.h
+//  StaticLibrary
+//
+//  Created by Wilson Yuan on 2017/3/8.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibrary : NSObject
+
+@end
